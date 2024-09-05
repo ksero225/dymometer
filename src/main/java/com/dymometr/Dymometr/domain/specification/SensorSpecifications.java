ca0343118536b521e1ff476dd9a3f2e3647ec18e
Paweł Michalski <pmpawel126@gmail.com>
2024-09-05 21:26:11 +0200

    comit

--- src/main/java/com/dymometr/Dymometr/domain/specification/SensorSpecifications.java
+++ src/main/java/com/dymometr/Dymometr/domain/specification/SensorSpecifications.java
@@ -5,10 +5,21 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class SensorSpecifications {
     public static Specification<SensorEntity> hasVoivodeship(String voivodeship) {
-        return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("sensorLocalization").get("sensorVoivodeship"), voivodeship));
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(
+                        criteriaBuilder.lower(
+                                root.get("sensorLocalization")
+                                        .get("sensorVoivodeship")),
+                        voivodeship.toLowerCase())
+        );
     }
 
     public static Specification<SensorEntity> hasTown(String town) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("sensorLocalization").get("sensorTown"), town);
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(criteriaBuilder.lower(
+                                root.get("sensorLocalization")
+                                        .get("sensorTown")),
+                        town.toLowerCase()
+                );
     }
 }
--- src/main/java/com/dymometr/Dymometr/repositories/SensorRepository.java
+++ src/main/java/com/dymometr/Dymometr/repositories/SensorRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SensorRepository extends JpaRepository<SensorEntity, Long>, JpaSpecificationExecutor<SensorEntity> {
 }
